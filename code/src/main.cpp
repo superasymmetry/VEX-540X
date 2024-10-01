b62ef993fd8aaf9ded9592d2463290dbdf54deab
superasymmetry <cokeymint@gmail.com>
2024-10-01 17:03:30 -0400

    Update main.cpp

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -52,7 +52,6 @@ void initialize() {
   	// sensor.calibrate();
 
 	// pros::Motor drive_left_initializer (MOTOR_PORT, E_MOTOR_GEARSET_18, true, E_MOTOR_ENCODER_DEGREES);
-	opcontrol();
 }
 
 /**
@@ -85,7 +84,7 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-
+	
 
 }
 
@@ -104,8 +103,8 @@ void autonomous() {
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, 2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({4, 5, 6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup left_mg({1, 2, -8});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({4, 5, -7});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
@@ -113,8 +112,8 @@ void opcontrol() {
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_LEFT_X);  // Gets the turn left/right from right joystick
+		int dir = master.get_analog(ANALOG_LEFT_X);    // Gets amount forward/backward from left joystick
+		int turn = master.get_analog(ANALOG_LEFT_Y);  // Gets the turn left/right from right joystick
 		pros::lcd::set_text(3, "Turn: " + std::to_string(turn) + " Dir: " + std::to_string(dir));
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
